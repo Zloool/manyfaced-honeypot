6a5eb3f9ffa7490094142d0c27828da118d0daa7
Zloool <zhavoronkov.p@gmail.com>
2016-10-12 22:37:10 +0300

    Some fixes
    Add settings.py
    + port
    + directory name
    Cleared code

--- runme.py
+++ runme.py
@@ -3,14 +3,20 @@ import datetime
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from cases import cases
 from httphandler import HTTPRequest
-# import signal
+from settings import HONEYPORT, HONEYFOLDER
 # TODO unittests
 
 
+# import signal
+# exit -- something to do on SIGINT
+# signal.signal(signal.SIGINT, exit)
+# TODO implement SIGINT handler
+
 def create_file(message, directory):
-    if not os.path.exists("bots/"+directory):
-        os.makedirs("bots/"+directory)
-    filename = "bots/"+directory+"/"+str(datetime.datetime.now()).replace(':', ';')
+    if not os.path.exists(HONEYFOLDER+directory):
+        os.makedirs(HONEYFOLDER+directory)
+    currtime = str(datetime.datetime.now()).replace(':', ';')
+    filename = HONEYFOLDER+directory+"/"+currtime
     f = open(filename, 'w')
     f.write(str(message))
     f.close()
@@ -33,14 +39,10 @@ def get_honey(path):
     f.close()
     return outputdata
 
-# exit -- something to do on SIGINT
-# signal.signal(signal.SIGINT, exit)
-# TODO implement SIGINT handler
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-# TODO add socketopt to resolve socket.error: [Errno 98] Address already in use
-serverSocket.bind(('', 80))
-# TODO args or settins
+serverSocket.bind(('', HONEYPORT))
+# TODO args
 serverSocket.listen(1)
 while True:
     connectionSocket, addr = serverSocket.accept()
@@ -52,32 +54,22 @@ while True:
         try:
             request = HTTPRequest(message)
             path = request.path
+            # TODO check error_code, instead of exc
         except:
             path = str(request.error_code)
-        # print request.error_code       # None  (check this first)
-        # print request.command          # "GET"
-        # print request.path             # "/who/ken/trust.html"
-        # print request.request_version  # "HTTP/1.1"
-        # print len(request.headers)     # 3
-        # print request.headers.keys()   # ['accept-charset', 'host', 'accept']
-        # print request.headers['host']  # "cm.bell-labs.com"
-        # filename = message.split()[1]
-        # print filename[1:]
-
         outputdata = get_honey(path)
-        connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n')
-        connectionSocket.send(outputdata)
 
-        # Send the content of the requested file to the client
-        # for i in range(0, len(outputdata)):
-        #    connectionSocket.send(outputdata[i])
+        connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n')
+        # TODO add server banner
+
+        connectionSocket.send(outputdata)
         connectionSocket.close()
     except IOError:
+        # TODO test and remove this branch
         print "Caught exception IOError"
         # connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n')
         connectionSocket.close()
     except socket.error, exc:
         print "Caught exception socket.error : %s" % exc
         connectionSocket.close()
-
 serverSocket.close()
