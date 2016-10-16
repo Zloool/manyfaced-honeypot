b7216660811ea50ea66fc919cb024b1a00dd5f18
Zloool <zhavoronkov.p@gmail.com>
2016-10-16 19:44:17 +0300

    Add documentation for httphandler

--- httphandler.py
+++ httphandler.py
@@ -6,6 +6,13 @@ class HTTPRequest(BaseHTTPRequestHandler):
     """
     Code from
     http://stackoverflow.com/questions/2115410/does-python-have-a-module-for-parsing-http-requests-and-responses
+
+    print request.command          # "GET"
+    print request.path             # "/who/ken/trust.html"
+    print request.request_version  # "HTTP/1.1"
+    print len(request.headers)     # 3
+    print request.headers.keys()   # ['accept-charset', 'host', 'accept']
+    print request.headers['host']  # "cm.bell-labs.com"
     """
 
     def __init__(self, request_text):
