05c203f5ec79054c6616a8f822ff20f2db0ae07b
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2016-10-21 17:29:21 +0300

    Make update interval equal to 1 hour

--- trigger.py
+++ trigger.py
@@ -4,7 +4,7 @@ import time
 
 def trigger(updateq):
     current_time = datetime.datetime.now()
-    delta = datetime.timedelta(seconds=10)
+    delta = datetime.timedelta(hours=1)
     while True:
         time.sleep(10)
         current_time += delta
