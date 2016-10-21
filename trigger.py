5b93706220de3671f7221381fe67b2c95931a0b2
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2016-10-21 18:21:29 +0300

    Actully make update interval equal to 1 hour
    The previous commit didn`t do the job

--- trigger.py
+++ trigger.py
@@ -6,6 +6,6 @@ def trigger(updateq):
     current_time = datetime.datetime.now()
     delta = datetime.timedelta(hours=1)
     while True:
-        time.sleep(10)
+        time.sleep(3600)
         current_time += delta
         updateq.put("update")
