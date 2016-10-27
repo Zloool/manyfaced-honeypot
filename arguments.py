b1b75dc30edec9ee6f269d8072e18dd59ea9271c
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2016-10-27 17:29:41 +0300

    Update docstring

--- arguments.py
+++ arguments.py
@@ -1,7 +1,7 @@
 import argparse
 
 """
-usage: mfh.py [-h] [-c | --client [PORT]] [-u] [-v]
+usage: mfh.py [-h] [-c | --client [PORT]] [-s | --server [PORT]] [-u] [-v]
 
 Serve some sweet honey to the ubiquitous bots!
 
@@ -9,8 +9,12 @@ optional arguments:
   -h, --help       show this help message and exit
   -c               launch client with on port defined in settings
   --client [PORT]  port to start a client on
+  -s               launch server with on port defined in settings
+  --server [PORT]  port to start a server on
   -u, --updater    enable self updating
   -v, --verbose    increase output verbosity
+
+And that`s how you`d detect a sneaky chinese bot.
 """
 
 
