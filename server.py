9d90fab77a44f3d96bdb387fe3f0dc061fbcded1
Zloool <zhavoronkov.p@gmail.com>
2016-10-28 13:50:39 +0300

    Fix: wrtie data to file only if clickhouse unavailable

--- server.py
+++ server.py
@@ -9,18 +9,23 @@ from settings import HIVEPORT, AUTHORISEDBEARS
 from myenc import AESCipher
 from dbconnect import Insert
 
-
-def main(args, update_event):
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocket.bind(('', HIVEPORT))
-    serverSocket.listen(1)
+def DumpToFile(data):
     try:
         with file('temp.db') as f:
             stringfile = f.read()
         db = pickle.loads(stringfile)
     except:
         db = list()
+    db.append(data)
+    with open('temp.db', "w") as f:
+        f.write(str(pickle.dumps(db)))
+
+
+def main(args, update_event):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serverSocket.bind(('', HIVEPORT))
+    serverSocket.listen(1)
     if args.verbose:
         print "Awaiting for bears on port %s" % HIVEPORT
     while True:
@@ -35,13 +40,12 @@ def main(args, update_event):
             data = pickle.loads(deciper.decrypt(request[1]))
             if args.verbose:
                 print data
-            db.append(data)
             try:
                 Insert(data)
             except:
-                print "Error writing data to clickhouse"
-            with open('temp.db', "w") as f:
-                f.write(str(pickle.dumps(db)))
+                DumpToFile(data)
+                if args.verbose:
+                    print "Error writing data to clickhouse, writing to file"
             connectionSocket.send("200")
             connectionSocket.close()
         except:
