be70f23d26390475abf1597744a99c0d24404dc7
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2016-10-28 14:07:27 +0300

    Rename mfhclient.py to client.py
    Files names are more consistent and understandable.

--- mfh.py
+++ mfh.py
@@ -4,7 +4,7 @@ import time
 
 from multiprocessing import Process, Event
 
-import mfhclient
+import client
 import server
 import update
 
@@ -14,21 +14,21 @@ from settings import HONEYPORT, HIVEPORT
 
 def main():
     update_event = Event()
-    client = create_process("client", mfhclient.main, args, update_event)
-    serv = create_process("server", server.main, args, update_event)
+    client_proc = create_process("client", client.main, args, update_event)
+    server_proc = create_process("server", server.main, args, update_event)
     if args.client is not None:
-        client.start()
+        client_proc.start()
     if args.client is not None:
-        serv.start()
+        server_proc.start()
     if args.updater:
         trigger = create_process("trigger", update.trigger, update_event)
         trigger.start()
         trigger.join()
-    while client.is_alive() or serv.is_alive():
+    while client_proc.is_alive() or server_proc.is_alive():
         time.sleep(5)
     else:
         if args.updater:
-            update.pull("origin", "master")
+            # update.pull("origin", "master")
             sys.stdout.flush()
             os.execl(sys.executable, sys.executable, *sys.argv)
 
@@ -36,7 +36,7 @@ def main():
 def create_process(name, function,  *arguments):
     process = Process(
         args=arguments,
-        name="{0}_process".format(name),
+        name=name,
         target=function,
         )
     return process
@@ -48,5 +48,4 @@ if __name__ == '__main__':
         args.client = HONEYPORT
     if args.s:
         args.server = HIVEPORT
-    processes = {}
     main()
