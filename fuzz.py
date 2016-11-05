ed935a2d92650c5b2b52358a1dccaef31326268d
Zloool <zhavoronkov.p@gmail.com>
2016-11-05 23:33:19 +0200

    Add some uranium to fuzzer

--- fuzz.py
+++ fuzz.py
@@ -3,21 +3,44 @@ import socket
 import random
 import string
 import time
+import multiprocessing
 
-HOST = '127.0.0.1'    # The remote host
-PORT = 80              # The same port as used by the server
-for i in range(1, 10000):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST, PORT))
-        N = i
-        ptr = string.ascii_uppercase + string.digits
-        ptr += "!@#$%^&*()_+`-~"
-        str = ''.join(random.choice(ptr) for _ in range(N))
-        s.sendall(str)
-        s.close()
-        print str, ':\r\n'  # repr(data)
-        time.sleep(0.001)
-    except:
-        print "babach"
-        time.sleep(1)
+
+def connect():
+    while True:
+        try:
+            HOST = '127.0.0.1'
+            PORT = 80
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            N = 100000
+            ptr = string.ascii_uppercase + string.digits
+            ptr += "!@#$%^&*()_+`-~"
+            request = 'GET /wp-login.php HTTP/1.1'
+            request += '\r\n'
+            request += 'Content-Type: text/html'
+            request += '\r\n'
+            request += 'Content-Length: 75'
+            request += '\r\n'
+            request += '\r\n'
+            request += ''.join(random.choice(ptr) for _ in range(N))
+            s.send(request)
+            print "-",
+        except:
+            print "!",
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    processes = []
+    for i in range(10):
+        processes.append(multiprocessing.Process(
+            target=connect,
+            args=()
+            )
+        )
+    for i in range(len(processes)):
+        processes[i].start()
+    for i in range(len(processes)):
+        processes[i].join()
+    time.sleep(100)
