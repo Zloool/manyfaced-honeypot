f445a3505cea7ba83a66671391fadf090877c496
Zloool <zhavoronkov.p@gmail.com>
2016-11-12 01:34:01 +0200

    Split main on more functions

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import signal
 from socket import (socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR,
                     error as sockerror)
 from multiprocessing import Process, Lock
+from operator import itemgetter
 
 
 from settings import HIVEHOST, HIVEPORT, HIVELOGIN, HIVEPASS
@@ -30,7 +31,6 @@ def send_report(data, client, password, lock):
             s.close()
         except sockerror:
             DumpToFile(data)
-            # response = "Hive server is not responding :("
         except KeyboardInterrupt:
             pass
     os._exit(0)
@@ -72,7 +72,7 @@ def compile_banner(msgsize=0,
     return banner
 
 
-def get_honey_http(args, request, ip_addr):
+def get_honey_http(request, ip_addr, verbose):
     """
     This is the place where magic happens. Function receives parsed HTTP
     request as an argument and returns an output as a string. If it
@@ -81,47 +81,43 @@ def get_honey_http(args, request, ip_addr):
     example, WEBDAV protocol uses different server banner and Content-Type of
     robots.txt should be text/plain(they are also dynamically generated).
     """
-    outputdata = ""
-    stringfile = ""
     if request.path in faces:  # If we know what to do with request
         respfilename = faces[request.path]
         if respfilename == "webdav.xml":  # Compile response for WEBDAV listing
             with file('responses/' + faces[request.path]) as f:
-                stringfile = f.read()
-            outputdata += compile_banner(code='HTTP/1.1 207 Multi-Status',
-                                         contenttype='application/xml; '
-                                         'charset=utf-8',
-                                         connection=0, date=0, serverver=0,
-                                         nlcount=1)
-            outputdata += stringfile
+                body = f.read()
+            outputdata = compile_banner(code='HTTP/1.1 207 Multi-Status',
+                                        contenttype='application/xml; '
+                                        'charset=utf-8',
+                                        connection=0, date=0, serverver=0,
+                                        nlcount=1)
+            outputdata += body
         elif respfilename == "robots":  # Generate robots.txt from faces dict
-            stringfile = 'User-Agent: *\r\nAllow: /\r\n'
+            body = 'User-Agent: *\r\nAllow: /\r\n'
             for url in known_faces:
-                stringfile += 'Disallow: ' + url + "\r\n"
-            outputdata += compile_banner(msgsize=len(stringfile),
-                                         contenttype="text/plain"
-                                         "; charset=UTF-8")
-            outputdata += stringfile
+                body += 'Disallow: ' + url + "\r\n"
+            outputdata = compile_banner(msgsize=len(body),
+                                        contenttype="text/plain"
+                                        "; charset=UTF-8")
+            outputdata += body
         else:  # If our request doesnt require special treatment, it goes here
             with file('responses/' + faces[request.path]) as f:
-                stringfile = f.read()
-            outputdata += compile_banner(msgsize=len(stringfile))
-            outputdata += stringfile
-        if args.verbose:
+                body = f.read()
+            outputdata = compile_banner(msgsize=len(body))
+            outputdata += body
+        if verbose:
             print ip_addr + " " + request.path + " gotcha!"
     else:  # If we dont know what to do with that request
-        if args.verbose:
+        if verbose:
             print ip_addr + " " + request.path[:50] + " not detected..."
         if request.path not in unknown_faces:
-            # Lets add him to our todo list ;) (if we dont already have it)
             unknown_faces.append(request.path)
             with open("local_faces.txt", "a") as f:
                 f.write(request.path + "\n")
-        # Send default response
         with file('responses/' + faces["zero"]) as f:
-            stringfile = f.read()
-        outputdata += compile_banner(msgsize=len(stringfile))
-        outputdata += stringfile
+            body = f.read()
+        outputdata = compile_banner(msgsize=len(body))
+        outputdata += body
     try:
         detected = map(itemgetter(0), faces).index(request.path)
     except:
@@ -129,35 +125,36 @@ def get_honey_http(args, request, ip_addr):
     return outputdata, detected
 
 
-def main(args, update_event):
-    if getattr(signal, 'SIGCHLD', None) is not None:
-        signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-    report_lock = Lock()
-    # Get our unimplemented requests list, so we can add something to it
-    global unknown_faces
-    if not os.path.isfile("local_faces.txt"):
-        f = file("local_faces.txt", "w")
-        f.close()
-    unknown_faces = [line.rstrip('\n') for line in open('faces.txt')]
-    unknown_faces += [line.rstrip('\n') for line in open('local_faces.txt')]
-    # Create a known_faces set, so we can generate robots.txt
-    global known_faces
-    known_faces = set()
-    for url in faces:
-        known_faces.add(url)
-    # Create socket
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    # Need to set setsockopt to resolve "port already in use" issue
-    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    # Bind our socket to a port
-    # Will use one from settings, if not given in args
-    serverSocket.bind(('', args.client))
-    serverSocket.listen(1)
-    if args.verbose:
-        print "Serving honey on port %s" % args.client
-    # Endless loop for handling requests
-    while True:
+def handle_request(message, request_time, bot_ip, verbose, report_lock):
+    request = HTTPRequest(message)
+    if request.error_code is None:
+        if hasattr(request, 'path'):
+            outputdata, detected = get_honey_http(request, bot_ip, verbose)
+        else:
+            outputdata = message
+            detected = 4294967295 - 1
+    else:
+        if verbose:
+            print "Got non-http request"
+        detected = 4294967295 - 2
+        outputdata = message
+    bs = BearStorage(bot_ip, unicode(message, errors='replace'),
+                     request_time, request, detected, HIVELOGIN)
+    Process(
+        args=(bs, HIVELOGIN, HIVEPASS, report_lock),
+        name="send_report",
+        target=send_report,).start()
+    return outputdata
 
+
+def create_server(port, report_lock, verbose, update_event):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serverSocket.bind(('', port))
+    serverSocket.listen(1)
+    if verbose:
+        print "Serving honey on port %s" % port
+    while True:
         if update_event.is_set():
             break
         try:
@@ -166,43 +163,38 @@ def main(args, update_event):
             if 'connectionSocket' in locals():
                 connectionSocket.close()
             break
-        # Need to use try, because socket will generate a lot of exceptions
         try:
-            # Argument is the number of bytes to recieve from client
             message = recv_timeout(connectionSocket, 0.25)
         except sockerror:
-            if args.verbose:
+            if verbose:
                 print "Failed to recieve data from bot"
             continue
-        ip_addr = connectionSocket.getpeername()[0]
-        dt = str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"))
-        # Try to parse request parameters from message
-        request = HTTPRequest(message)
-        if request.error_code is None:
-            if hasattr(request, 'path'):
-                outputdata, detected = get_honey_http(
-                    args, request, ip_addr)
-            else:
-                outputdata = message
-                detected = 4294967295 - 1
-        # If it's not an HTTP request, it goes here
-        else:
-            if args.verbose:
-                print "Got non-http request"
-            detected = 4294967295 - 2
-            outputdata = message
-        bs = BearStorage(ip_addr, unicode(message, errors='replace'),
-                         dt, request, detected, HIVELOGIN)
-        response = Process(
-            args=(bs, HIVELOGIN, HIVEPASS, report_lock),
-            name="send_report",
-            target=send_report,)
-        response.start()
+        bot_ip = connectionSocket.getpeername()[0]
+        request_time = str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"))
+        outputdata = handle_request(message, request_time, bot_ip,
+                                    verbose, report_lock)
         try:
             connectionSocket.send(outputdata)
             connectionSocket.close()
         except sockerror:
-            if args.verbose:
+            if verbose:
                 print "Failed to send response to bot"
             continue
     serverSocket.close()
+
+
+def main(args, update_event):
+    if getattr(signal, 'SIGCHLD', None) is not None:
+        signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+    report_lock = Lock()
+    global unknown_faces
+    if not os.path.isfile("local_faces.txt"):
+        f = file("local_faces.txt", "w")
+        f.close()
+    unknown_faces = [line.rstrip('\n') for line in open('faces.txt')]
+    unknown_faces += [line.rstrip('\n') for line in open('local_faces.txt')]
+    global known_faces
+    known_faces = set()
+    for url in faces:
+        known_faces.add(url)
+    create_server(args.client, report_lock, args.verbose, update_event)
