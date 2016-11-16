1e35dc595541db4bce14d4af4506282ecb04e072
Zloool <zhavoronkov.p@gmail.com>
2016-11-16 12:07:54 +0200

    Move some funcions to common.utils

--- manyfaced/server/server.py
+++ manyfaced/server/server.py
@@ -1,28 +1,15 @@
 import pickle
-import time
 import os
 import signal
 from multiprocessing import Process, Lock
 from requests.exceptions import ConnectionError
 from socket import (socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR,
-                    error as sockerror)
+                    error as socket_error)
 
-from common.status import CLIENT_TIMEOUT
 from common.myenc import AESCipher
 from common.settings import AUTHORISEDBEARS
+from common.utils import dump_file, recv_timeout
 from db.dbconnect import Insert
-
-
-def dump_file(data):
-    try:
-        with file('temp.db') as f:
-            string_file = f.read()
-        db = pickle.loads(string_file)
-    except:
-        db = list()
-    db.append(data)
-    with open('temp.db', "w") as f:
-        f.write(str(pickle.dumps(db)))
 
 
 def data_saving(data, args, lock):
@@ -36,41 +23,6 @@ def data_saving(data, args, lock):
         except KeyboardInterrupt:
             pass
     os._exit(0)
-
-
-def recv_timeout(the_socket, timeout=CLIENT_TIMEOUT):
-    # make socket non blocking
-    the_socket.setblocking(0)
-
-    # total data partwise in an array
-    total_data = []
-
-    # beginning time
-    begin = time.time()
-    while True:
-        # if you got some data, then break after timeout
-        if total_data and time.time() - begin > timeout:
-            break
-
-        # if you got no data at all, wait a little longer, twice the timeout
-        elif time.time() - begin > timeout * 2:
-            break
-
-        # recv something
-        try:
-            data = the_socket.recv(8192)
-            if data:
-                total_data.append(data)
-                # change the beginning time for measurement
-                begin = time.time()
-            else:
-                # sleep for sometime to indicate a gap
-                time.sleep(0.1)
-        except:
-            pass
-
-    # join all parts to make final string
-    return ''.join(total_data)
 
 
 def main(args, update_event):
@@ -110,7 +62,7 @@ def main(args, update_event):
                 target=data_saving
             ).start()
             connection_socket.send("200")
-        except sockerror, e:
+        except socket_error, e:
             print e
             continue
         except TypeError, e:
