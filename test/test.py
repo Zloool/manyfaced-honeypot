f09fce71cb5d113c87e62ee28bf4f588ac92b8c9
Zloool <zhavoronkov.p@gmail.com>
2016-12-23 00:37:59 +0200

    Fix test conflict with settings.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,16 @@
+import os
+import sys
+from shutil import copyfile
+
 from manyfaced.client import faces
 from manyfaced.client import client
+
+
+def test_create_config():
+    if not os.path.isfile(os.path.join(sys.path[0], "manyfaced", "common", "settings.py")):
+        copyfile(os.path.join(sys.path[0], "manyfaced", "common", "settings.py.example"),
+                 os.path.join(sys.path[0], "manyfaced", "common", "settings.py"))
+    pass
 
 
 def test_gethoney():
