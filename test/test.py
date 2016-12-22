321b20c04765202c3d6fb0385dcc0212c0823968
Zloool <zhavoronkov.p@gmail.com>
2016-12-23 00:42:36 +0200

    Fix import order in settings

--- test/test.py
+++ test/test.py
@@ -2,15 +2,17 @@ import os
 import sys
 from shutil import copyfile
 
-from manyfaced.client import faces
-from manyfaced.client import client
-
-
 def test_create_config():
     if not os.path.isfile(os.path.join(sys.path[0], "manyfaced", "common", "settings.py")):
         copyfile(os.path.join(sys.path[0], "manyfaced", "common", "settings.py.example"),
                  os.path.join(sys.path[0], "manyfaced", "common", "settings.py"))
     pass
+
+from manyfaced.client import faces
+from manyfaced.client import client
+
+
+
 
 
 def test_gethoney():
