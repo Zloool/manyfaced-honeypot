b5fd3ded20de7c24cd9904e6a3982a08cf185ea9
Zloool <you@example.com>
2026-04-18 22:52:18 +0300

    Expose client functions in package __init__
    Import and re-export client utilities (compile_banner, faces, get_honey_http, honey_generic) in manyfaced.client.__init__.py and add them to the package API so they can be accessed directly from the manyfaced.client package.

--- manyfaced/client/__init__.py
+++ manyfaced/client/__init__.py
@@ -1,5 +1,12 @@
 """manyfaced client package."""
 
+from manyfaced.client.client import (
+    compile_banner,
+    faces,
+    get_honey_http,
+    honey_generic,
+)
+
 __all__ = [
     "compile_banner",
     "faces",
